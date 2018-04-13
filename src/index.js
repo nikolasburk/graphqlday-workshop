36bbe339fbaa14e67fbd9454712fdd3f3d555d06
Nikolas Burk <nikolas.burk@gmail.com>
2018-04-13 12:16:06 +0200

    start server

--- src/index.js
+++ src/index.js
@@ -11,3 +11,9 @@ const resolvers = {
     info: () => `This is the API for a simple blogging application.`
   }
 }
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+server.start(() => console.log(`GraphQL server is running on http://localhost:4000`))
