2161152ff84d14b1ef5933180e96fb9cf63738fa
Nikolas Burk <nikolas.burk@gmail.com>
2018-04-14 07:02:12 +0200

    finalize Prisma setup and implement first query

--- README.md
+++ README.md
@@ -1,30 +1,67 @@
-# Step 2
+# Step 3
 
 Go back to the [`master`](https://github.com/nikolasburk/graphqlday-workshop) branch.
 
 ## Usage
 
+### Deploy the Prisma service
+
+```bash
+npm install -g prisma
+prisma deploy
+```
+
+> **Note**: If you don't have [Docker](https://www.docker.com) installed on your machine, you need to remove the [`cluster`](./database/prisma.yml#L9) property from [`prisma.yml`]((./database/prisma.yml) and select a _development cluster_ when prompted by the CLI where to deploy your Prisma API. The endpoint that's then printed by the CLI needs to be pasted into [`index.js`](./src/index.js#L29) where `Prisma` is instantied.
+
+### Start the server
+
 ```bash
 node src/index.js
 ```
 
+### Open a GraphQL Playground
+
+```bash
+npm install -g graphql-cli
+graphql playground
+```
+
 ## Sample queries/mutations
 
+> In the following queries/mutation, `__POST_ID__` is a placeholder that needs to be replaced with the `id` of an actual `Post` item in your database.
+
+### Application layer (`graphql-yoga`)
+
 ```graphql
-post(id: "post-0") {
-  id
-  title
-  content
-  published
+query {
+  posts {
+    id
+    title
+    content
+    published
+  }
 }
 ```
 
 ```graphql
+query {
+  posts(searchString: "QL") {
+    id
+    title
+    content
+    published
+  }
+}
+```
+
+### Database layer (Prisma)
+
+```graphql
 mutation {
-  createDraft(
-    title: ""
-    content: ""
-  ) {
+  createPost(data: {
+    title: "GraphQL is great"
+    content: "It really is"
+  }) {
     id
     published
   }
@@ -32,17 +69,27 @@ mutation {
 ```
 
 ```graphql
-mutation {
-  publish(id: "post-0") {
+query {
+  posts(where: {
+    OR: [{
+      title_contains: "QL"
+      }, {
+      content_contains: "QL"
+    }]
+  }) {
     id
+    title
+    content
     published
   }
 }
 ```
 
 ```graphql
-mutation {
-  deletePost(id: "post-0") {
+query {
+  post(where: {
+    id: "__POST_ID__"
+  }) {
     id
     title
     content
--- src/index.js
+++ src/index.js
@@ -1,61 +1,51 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-const postData = [
-  {
-    id: 'post-0',
-    title: '',
-    content: '',
-    published: false,
-  },
-]
-let idCount = postData.length
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
     info: () => `This is the API for a simple blogging application.`,
-    posts: (_, args) => {
-      return args.searchString
-        ? postData.filter(
-            post =>
-              post.title.includes(args.searchString) ||
-              post.content.includes(args.searchString),
-          )
-        : postData
+    posts: (_, args, context, info) => {
+      return context.db.query.posts(
+        {
+          where: {
+            OR: [
+              {
+                title_contains: args.searchString,
+              },
+              {
+                content_contains: args.searchString,
+              },
+            ],
+          },
+        },
+        info,
+      )
     },
-    post: (_, args) => {
-      return postData.find(post => post.id === args.id)
-    }
+    // post: (_, args, context, info) => {
+    // }
   },
-  Mutation: {
-    createDraft: (_, args) => {
-      const newDraft = {
-        id: `post-${idCount++}`,
-        title: args.title,
-        content: args.content,
-        published: false
-      }
-      postData.push(newDraft)
-      return newDraft
-    },
-    publish: (_, args) => {
-      const postToPublish = postData.find(post => post.id === args.id)
-      postToPublish.published = true
-      return postToPublish
-    },
-    deletePost: (_, args) => {
-      const postToDeleteIndex = postData.findIndex(post => post.id === args.id)
-      if (postToDeleteIndex > -1) {
-        const deleted = postData.splice(postToDeleteIndex, 1)
-        return deleted[0]
-      }
-      return null
-    }
-  }
+  // Mutation: {
+  //   createDraft: (_, args, context, info) => {
+  //   },
+  //   publish: (_, args, context, info) => {
+  //   },
+  //   deletePost: (_, args, context, info) => {
+  //   }
+  // }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: './src/generated/prisma.graphql',
+      endpoint: 'http://localhost:4466/graphqlday-demo/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 })
 server.start(() =>
   console.log(`GraphQL server is running on http://localhost:4000`),
